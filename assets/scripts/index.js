d6052d6cee611ba16073b8255ae7c0ba0593a411
“wilsonkaya” <fatihkaya2@yahoo.com>
2017-03-26 13:19:19 -0400

    Implement auth
    Create  Sign-in Sing-up, Sing-out and change password in html.
    Create auth api, ui and events
    Run JQuery file when DOM is ready
    
    All crud actions have been tested and everything is working.
    
    Next is to deploy if front-end and back-end are working in sync.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,6 +2,7 @@
 
 const setAPIOrigin = require('../../lib/set-api-origin')
 const config = require('./config')
+const authEvents = require('./auth/events.js');
 
 $(() => {
   setAPIOrigin(location, config)
@@ -9,6 +10,9 @@ $(() => {
 
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
+$(() => {
+  authEvents.addHandlers()
+})
 
 // use require without a reference to ensure a file is bundled
 require('./example')
